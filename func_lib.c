4fc0e4351a46fb4c1f4ebab10ab807131d68a388
towasme <towasm@gmail.com>
2020-01-03 23:00:06 +0000

    added func add, swap

--- func_lib.c
+++ func_lib.c
@@ -104,25 +104,54 @@ void *op_pint(stack_t **stack, unsigned int num)
 
 
 /**¬
-*op_push - calls different functions¬
+*op_swap - calls different functions¬
 *@stack: pointer¬
-*@line number: number to manipulate¬
+*@num: number to manipulate¬
 *Return: Always¬
 */
-/*
+
 void *op_swap(stack_t **stack, unsigned int num)
 {
+	UNUSED (num);
+	int num_swap = 0;
+
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L<line_number>: can't swap, stack too short");
+		exit (EXIT_FAILURE);
+	}
+	else
+	{
+		num_swap = (*stack)->n;
+		(*stack)->n = (*stack)->next->n;
+		(*stack)->next->n = num_swap
+	}
 }
 
-*/
 /**¬
-  6  *op_push - calls different functions¬
-  7  *@stack: pointer¬
-  8  *@line number: number to manipulate¬
-  9  *Return: Always¬
- 10  */
-/*
+*op_add - calls different functions¬
+*@stack: pointer¬
+*@num: number to manipulate¬
+*Return: Always¬
+*/
+
 void *op_add(stack_t **stack, unsigned int num)
 {
+	UNUSED (num);
+	int sum_num = 0;
+	stack_t *aux;
+
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L<line_number>: can't add, stack too short");
+		exit (EXIT_FAILURE);
+	}
+	else
+	{
+	sum_num = (*stack)->n;
+	(*stack)->next->n += sum_num;
+	*stack = aux->next;¬
+	(*stack)->prev = NULL;¬
+	free(aux);
+	}
 }
-*/
