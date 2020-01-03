b5aadd414808db01cf62226760efed48b8fe8489
towasme <towasm@gmail.com>
2020-01-03 21:21:25 +0000

    added:pint, pop and pall

--- func_lib.c
+++ func_lib.c
@@ -5,7 +5,7 @@
 /**
  *op_push - calls different functions
  *@stack: pointer
- *@line number: number to manipulate
+ *@num: number to manipulate
  *Return: Always
  */
 
@@ -35,38 +35,73 @@ void *op_push(stack_t **stack, unsigned int num)
 		*stack = new;
 	}
 }
-
 /**¬
-  6  *op_push - calls different functions¬
+  6  *op_pop - calls different functions¬
   7  *@stack: pointer¬
-  8  *@line number: number to manipulate¬
+  8  *@num: number to manipulate¬
   9  *Return: Always¬
  10  */
 
 void *op_pop(stack_t **stack, unsigned int num)
 {
+	stack_t *aux = *stack;
+	UNUSED (num);
+
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L<line_number>: can't pop an empty stack\n")
+		exit (EXIT_FAILURE);
+	}
+	else
+	{
+		*stack = aux->next;
+		(*stack)->prev = NULL;
+		free(aux);
+	}
 }
 
 /**¬
-  6  *op_push - calls different functions¬
-  7  *@stack: pointer¬
-  8  *@line number: number to manipulate¬
-  9  *Return: Always¬
- 10  */
+*op_pall - calls different functions¬
+*@stack: pointer¬
+*@num: number to manipulate¬
+*Return: Always¬
+*/
+
 void *op_pall(stack_t **stack, unsigned int num)
 {
+	int counter = 0;
+	UNUSED (num);
+
+	while (stack)
+	{
+		printf("%d\n", (*stack)->n);
+		*stack = (*stack)->next;
+		counter++;
+	}
 }
 
 /**¬
-*op_push - calls different functions¬
+*op_pint - calls different functions¬
 *@stack: pointer¬
-*@line number: number to manipulate¬
+*@num: number to manipulate¬
 *Return: Always¬
 */
 
 void *op_pint(stack_t **stack, unsigned int num)
 {
+	UNUSED (num);
+
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L<line_number>: can't pint, stack empty\n");
+		exit (EXIT_FAILURE);
+	}
+	else
+	{
+		printf("%d\n", (*stack)->n);
+	}
 }
+
 
 /**¬
 *op_push - calls different functions¬
@@ -74,18 +109,20 @@ void *op_pint(stack_t **stack, unsigned int num)
 *@line number: number to manipulate¬
 *Return: Always¬
 */
-
+/*
 void *op_swap(stack_t **stack, unsigned int num)
 {
 }
 
+*/
 /**¬
   6  *op_push - calls different functions¬
   7  *@stack: pointer¬
   8  *@line number: number to manipulate¬
   9  *Return: Always¬
  10  */
-
+/*
 void *op_add(stack_t **stack, unsigned int num)
 {
 }
+*/
